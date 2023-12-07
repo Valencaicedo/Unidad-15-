f7b7ac8e13f59d28212d3866e435be8c21bcce15
Valencaicedo <138271675+Valencaicedo@users.noreply.github.com>
2023-12-07 18:14:31 -0500

    Add files via upload

---
+++ src/main/java/com/app/repositories/DoctorRepository.java
@@ -0,0 +1,10 @@
+package com.app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.entities.DoctorEntity;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<DoctorEntity, Long> {
+}
